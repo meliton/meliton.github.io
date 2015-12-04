8dce3af1145f1443a2006782816b585088db978b
Mel <m_hinojosa@rocketmail.com>
2015-12-04 13:35:36 -0600

    Update testinstall.sh

--- testinstall.sh
+++ testinstall.sh
@@ -8,24 +8,20 @@
 #
 # curl -L meliton.github.io\testinstall.sh | bash
 
-
-# Get the screen size in case we need a full-screen message and so we can display dialog that is sized the best
-#screenSize=$(stty -a | tr \; \\012 | grep 'rows|columns' | cut '-d ' -f3)
-
 # Find rows and columns
 rows=$(stty -a | tr \; \\012 | grep 'rows' | cut -d' ' -f3)
 columns=$(stty -a | tr \; \\012 | grep 'columns' | cut -d' ' -f3)
 
-# Divide by two so the dialogs take up half of the screen, which looks nice.
+# Divide by two so the dialogs take up half of the screen
 r=$(( rows / 2 ))
 c=$(( columns / 2 ))
 
 
 # Display the welcome dialog message
-whiptail --msgbox "This installer will allow you to add features to your My Cloud NAS" --backtitle "Welcome" --title "WD My Cloud Automated Installer"  $r $c
+whiptail --backtitle "Welcome" --title "WD My Cloud Automated Installer" --msgbox "This installer will allow you to add features to your My Cloud NAS" $r $c
 
 # Inform about device specs
-whiptail --msgbox "We will first check that you are running on a compatible device" --backtitle "Checking Device Specs" --title "Part 2" $r $c
+whiptail --backtitle "Checking Device Specs" --title "Verifying Device" --msgbox "We will first check that you are running on a compatible device" $r $c
 
 
 echo ; echo ; echo This is a sample installer
