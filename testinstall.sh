8d46c189c4c179a3c894fb1d8264b4f80428806e
Mel <m_hinojosa@rocketmail.com>
2015-12-04 10:44:54 -0600

    Added intro

--- testinstall.sh
+++ testinstall.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# WD My Cloud Installer
+# by Meliton Hinojosa
+# Install programs to your WD MyCLoud
+#
+#
+# Install with this command (from your WD My Cloud):
+#
+# curl -L meliton.github.io\testinstall.sh | bash
+
+
 echo ; echo ; echo This is a sample installer
 echo ; echo ; echo The end.
