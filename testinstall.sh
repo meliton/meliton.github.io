fa2ae0c16872a22dfde93954c686efb86ca99580
Mel <m_hinojosa@rocketmail.com>
2015-12-04 14:02:03 -0600

    Break script into functions

--- testinstall.sh
+++ testinstall.sh
@@ -8,6 +8,13 @@
 #
 # curl -L meliton.github.io\testinstall.sh | bash
 
+########## VARIABLES ##########
+# put vars here
+
+
+########## FUNCTIONS ##########
+getRowColumn()
+{
 # Find rows and columns
 rows=$(stty -a | tr \; \\012 | grep 'rows' | cut -d' ' -f3)
 columns=$(stty -a | tr \; \\012 | grep 'columns' | cut -d' ' -f3)
@@ -15,13 +22,24 @@ columns=$(stty -a | tr \; \\012 | grep 'columns' | cut -d' ' -f3)
 # Divide by two so the dialogs take up half of the screen
 r=$(( rows / 2 ))
 c=$(( columns / 2 ))
+}
 
-
+welcomeDialog()
+{
 # Display the welcome dialog message
 whiptail --backtitle "Welcome" --title "WD My Cloud Automated Installer" --msgbox "This installer will allow you to add features to your My Cloud NAS" $r $c
 
 # Inform about device specs
 whiptail --backtitle "Checking Device Specs" --title "Verifying Device" --msgbox "We will first check that you are running on a compatible device" $r $c
+}
+
+
+########## SCRIPT ##########
+# get screen info to render properly
+getRowColumn
+
+# Welcome screen
+welcomeDialog
 
 
 echo ; echo ; echo This is a sample installer
