a14313416dceacc8865a722034ec55eef54f8fb4
Mel <m_hinojosa@rocketmail.com>
2015-12-10 08:03:14 -0600

    Update loginsense.sh

--- loginsense.sh
+++ loginsense.sh
@@ -28,19 +28,9 @@ loginDialog()
 
 USER=$(whiptail --inputbox "Enter your user name" $r $c --title "pfSense Login Screen" 3>&1 1>&2 2>&3)
 exitstatus=$?
-if [ $exitstatus -eq 1 ]; then
-    exit 1
-else
-    echo 
-fi
 
 PASSWORD=$(whiptail --passwordbox "Enter your password" $r $c --title "pfSense Login Screen" 3>&1 1>&2 2>&3)
 exitstatus=$?
-if [ $exitstatus -eq 1 ]; then
-    exit 1
-else
-    echo 
-fi
 
 curl --connect-timeout 5 --data "auth_user=$varUser&auth_pass=$varPass&accept=Continue" http://192.168.1.1:8002
 }
