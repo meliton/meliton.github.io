a87345e697d985be23c9b91e2cd656b9cad28622
Mel <m_hinojosa@rocketmail.com>
2015-12-10 07:49:12 -0600

    Create loginsense.sh

---
+++ loginsense.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+# pfSense Login Script
+# by Meliton Hinojosa
+#
+# Log into pfSense
+#
+# curl -L meliton.github.io\loginsense.sh | bash
+
+########## VARIABLES ##########
+# put vars here
+
+
+########## FUNCTIONS ##########
+getRowColumn()
+{
+# Find rows and columns
+rows=$(stty -a | tr \; \\012 | grep 'rows' | cut -d' ' -f3)
+columns=$(stty -a | tr \; \\012 | grep 'columns' | cut -d' ' -f3)
+
+# Divide by two so the dialogs take up half of the screen
+r=$(( rows / 2 ))
+c=$(( columns / 2 ))
+}
+
+loginDialog()
+{
+# display the login dialog
+
+USER=$(whiptail --inputbox "Enter your user name" $r $c Blue --title "pfSense Login Screen" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "User selected Ok and entered " $USER
+else
+    echo "User selected Cancel."
+fi
+echo "(Exit status was $exitstatus)"
+
+PASSWORD=$(whiptail --passwordbox "Enter your password" $r $c --title "pfSense Login Screen" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "User selected Ok and entered " $PASSWORD
+else
+    echo "User selected Cancel."
+fi
+echo "(Exit status was $exitstatus)"
+
+curl --connect-timeout 5 --data "auth_user=$varUser&auth_pass=$varPass&accept=Continue" http://192.168.1.1:8002
+}
+
+########## SCRIPT ##########
+# get screen info to render properly
+getRowColumn
+
+# login screen
+loginDialog
