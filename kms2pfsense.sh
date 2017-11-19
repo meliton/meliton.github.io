aa89a373cc0a304a2c35552d505a0a894ff3c53b
Mel <m_hinojosa@rocketmail.com>
2017-11-19 15:55:43 -0600

    Update kms2pfsense.sh

--- kms2pfsense.sh
+++ kms2pfsense.sh
@@ -7,4 +7,35 @@
 # Install with this command (from your pfSense appliance):
 #
 # curl -L meliton.github.io/kms2pfsense.sh | sh
-echo Hello world
+getPFspecs()
+{
+# Notice to gather specs
+echo "First, we will check that you are running this on a compatible device."
+
+# check for FreeBSD OS
+case "$(uname -s 2>/dev/null | grep -c "FreeBSD" )" in
+   1) ;;
+   *) echo "Wrong OS type, not FreeBSD." ; 
+   exit 1 ;;
+esac
+
+# check for amd64 hardware
+case "$(uname -m 2>/dev/null | grep -c "amd64" )" in
+   1) ;;
+   *) echo "Wrong Hardware Type, not amd64." ; 
+   exit 1 ;;
+esac
+
+# check for OS version 11.xx or higher
+case "$(uname -r 2>/dev/null | grep -c "11." )" in
+   1) ;;
+   *) echo "Wrong OS version. Not at least 11.xx" ; 
+   exit 1 ;;
+esac
+
+echo "Success .You are running this on a compatible pfSense appliance." 
+}
+
+
+# get the hardware specs
+getPFspecs
