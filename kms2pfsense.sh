b7f4bd56d4aa3f89bf3ff2bb253de5f6cc455d1f
Mel <m_hinojosa@rocketmail.com>
2017-11-19 15:11:32 -0600

    Create kms2pfsense.sh

---
+++ kms2pfsense.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# KMS on pfSense Installer
+# by Meliton Hinojosa
+# Install KMS server on your pfSense appliance
+#
+#
+# Install with this command (from your pfSense appliance):
+#
+# curl -L meliton.github.io\kms2pfsense.sh | bash
+echo Hello world
